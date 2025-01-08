d88d047283b8a80f99f3c4c19a6e6029fce0a8a9
Shreya Kumari <shry1012005@gmail.com>
2025-01-08 21:43:07 +0530

    incredecre.cpp
    C++ program for the implementation of increment and decrement operators.

---
+++ incredecre.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"a:";
+    cin>>a;
+    cout<<a++<<endl;
+    cout<<"b:";
+    cin>>b;
+    cout<<--b<<endl;
+    c=a+5;
+    cout<<c<<endl;
+    return 0;
+}
