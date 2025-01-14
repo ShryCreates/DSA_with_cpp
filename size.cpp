b5826cd7c7571085ac59dafd048a714ea6837705
Shreya Kumari <shry1012005@gmail.com>
2025-01-14 21:45:56 +0530

    size.cpp
    C++ program to obtain the size of various data types.

---
+++ size.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int apples=5;
+    cout<<apples<<endl;
+    cout<<"Size of int:"<<sizeof(apples)<<endl;
+    char sign='a';
+    cout<<sign<<endl;
+    cout<<"Size of character:"<<sizeof(sign)<<endl;
+    bool flag=true;
+    cout<<flag<<endl;
+    cout<<"Size of boolean:"<<sizeof(flag)<<endl;
+    float score=3.14;
+    cout<<score<<endl;
+    cout<<"Size of float:"<<sizeof(score)<<endl;
+    double point=4.876;
+    cout<<point<<endl;
+    cout<<"Size of double:"<<sizeof(point)<<endl;
+    return 0;
+}
