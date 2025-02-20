951ec55ae68341b43e67ca0790ca5b3dae41828b
Shreya Kumari <shry1012005@gmail.com>
2025-02-20 13:03:43 +0530

    f19.cpp

---
+++ f19.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+string name="Shreya";
+void fun(){
+    cout<<name<<endl;
+}
+int main(){
+    cout<<name<<endl;
+    name="Kumari";
+    fun();
+    return 0;
+}
