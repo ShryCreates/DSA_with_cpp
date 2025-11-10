dbd3af2f754957f545a67bd927bc5937b32734c3
Shreya Kumari <shry1012005@gmail.com>
2025-11-10 22:42:48 +0530

    Update armstrong.cpp

--- armstrong.cpp
+++ armstrong.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     cout<<"Armstrong no. in the range of 100 to 500 are:"; //153=1^3+5^3+3^3(armstrong no.)
-    for(int i=0;i<=500;i++){
+    for(int i=100;i<=500;i++){
         int sum=0;
         int t=i;
         while(t!=0){
